7cc2097cebbf346afe62bf869537a1c470e5df93
liuyx <867947704@qq.com>
2020-04-10 17:02:23 +0800

    Update thswebviewpage.js

--- www/thswebviewpage.js
+++ www/thswebviewpage.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (url, success, error) {
+exports.startWebViewPage = function (url, success, error) {
     exec(success, error, 'thswebviewpage', 'startWebViewPage', [url]);
 };
