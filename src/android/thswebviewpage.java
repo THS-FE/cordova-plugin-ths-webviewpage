e42535269037bca79906a44181acbcb4e8ac9d7e
liuyx <867947704@qq.com>
2020-04-10 16:45:16 +0800

    完善功能

--- src/android/thswebviewpage.java
+++ src/android/thswebviewpage.java
@@ -6,6 +6,8 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import cn.com.ths.thswebview.ThsWebViewActivity;
+import android.content.Intent;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -14,17 +16,20 @@ public class thswebviewpage extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("startWebViewPage")) {
+            String url = args.getString(0);
+            this.startWebViewPage(url, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    private void startWebViewPage(String url, CallbackContext callbackContext) {
+        if (url != null && url.length() > 0) {
+            Intent  intent = new Intent(cordova.getActivity(), ThsWebViewActivity.class);
+            intent.putExtra(ThsWebViewActivity.EXTRA_STR,url);
+            cordova.getActivity().startActivity(intent);
+            callbackContext.success("success");
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
